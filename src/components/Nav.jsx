cd4059b4c53642ee008d06300cb7bdcf927ab442
Jack Yu <jackyu@Jacks-MacBook-Pro-711.local>
2026-08-09 15:13:41 -0700

    performance

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,7 +17,7 @@ export default function Nav() {
       <div className={`nav-links${open ? ' open' : ''}`}>
         <a href="/#about">About</a>
         <a href="/research">Research</a>
-        <a href="/resume">Résumé</a>
+        <a href="/resume">Resume</a>
         <a href="/contact">Contact</a>
         <a href="/creative">Creative</a>
         {/* LinkedIn link */}
